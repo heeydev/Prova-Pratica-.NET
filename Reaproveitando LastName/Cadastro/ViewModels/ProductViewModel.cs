42dc8cb1f61b160662e851dd5c0ffb695b94549b
heeydev <maciellbranco@hotmail.comgit config --global user.email maciellbranco@hotmail.comgit config --global user.email maciellbranco@hotmail.com>
2022-10-21 15:02:17 -0300

    Update Prova .NET

---
+++ Reaproveitando LastName/Cadastro/ViewModels/ProductViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Cadastro.Domain.Entities;
+using Cadastro.Services;
+
+namespace Cadastro.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Key]
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "O código do produto é requerido.")]
+        public int Id { get; set; }     
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O nome é requerido.")]
+        public string LastName { get; set; }
+        
+        [Display(Name = "Produto")]
+        [Required(ErrorMessage = "O nome é requerido.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Valor")]
+        [Required(ErrorMessage = "O valor é requerido.")]
+        public string Value { get; set; }        
+
+        [Display(Name = "Disponível")]
+        public bool Active { get; set; }
+    }
+}
